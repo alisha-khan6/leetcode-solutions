3de45770caa01c2ce7b814d8b3fee388dd46d6e9
Alisha Khan <alishakhan61107@gmail.com>
2026-07-09 15:40:05 +0000

    Sync LeetCode submission - Move Zeroes - Runtime - 0 ms (100.00%), Memory - 23.9 MB (19.82%)

--- solutions/0283-move-zeroes/solution.cpp
+++ solutions/0283-move-zeroes/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int j = 0 ;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(nums[i] != 0){
-                nums[j] = nums[i] ;
-                j++ ;
+        int i = 0 ;
+        for(int j = 0 ; j < nums.size() ; j++){
+            if(nums[j] != 0){
+                swap(nums[j] ,nums[i]);
+                i++ ;
             }
         }
-        while(j < nums.size()){
-            nums[j] = 0;
-            j++ ;
-        }
-        
     }
 };
