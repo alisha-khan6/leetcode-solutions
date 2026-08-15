15c3778269e7f2e8b43a5bd4cef8097a1a357509
Alisha Khan <alishakhan61107@gmail.com>
2026-08-15 10:02:05 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 0 ms (100.00%), Memory - 8.5 MB (84.07%)

--- solutions/0007-reverse-integer/solution.cpp
+++ solutions/0007-reverse-integer/solution.cpp
@@ -1,4 +1,3 @@
-#include <climits>
 class Solution {
 public:
     int reverse(int x) {
@@ -6,7 +5,7 @@ public:
         while (x != 0){
             int rem = x % 10 ;
             if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10) {
-                return 0; 
+                return 0; // Return 0 as required by LeetCode rules
             }
             reverse = reverse * 10 + rem ;
             x = x / 10 ; 
