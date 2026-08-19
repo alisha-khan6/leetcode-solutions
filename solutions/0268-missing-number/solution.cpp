e2cc2d277760f9b08f8c6f56db662255a9a7afa0
Alisha Khan <alishakhan61107@gmail.com>
2026-08-19 15:26:55 +0000

    Sync LeetCode submission - Missing Number - Runtime - 0 ms (100.00%), Memory - 21.9 MB (38.89%)

--- solutions/0268-missing-number/solution.cpp
+++ solutions/0268-missing-number/solution.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin() , nums.end());
-        // int a = -1;
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] != i){
-               return i ;
+                return i ;
             }
         }
-    return nums.size();
+        return nums.size();
     }
 };
