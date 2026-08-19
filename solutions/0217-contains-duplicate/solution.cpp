20feeaa1d927e28240add54a75051054baea5984
Alisha Khan <alishakhan61107@gmail.com>
2026-08-19 15:06:39 +0000

    Sync LeetCode submission - Contains Duplicate - Runtime - 27 ms (86.59%), Memory - 82.8 MB (79.89%)

--- solutions/0217-contains-duplicate/solution.cpp
+++ solutions/0217-contains-duplicate/solution.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       sort(nums.begin() , nums.end());
-       for(int i = 0 ; i < nums.size() - 1 ; i++){
-        if(nums[i] == nums[i+1]){
-            return true ;
+        sort(nums.begin() , nums.end()) ;
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i-1] == nums[i]){
+                return true ;
+            }
         }
-       } 
-       return false;
+        return false ;
     }
 };
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//        int i = 0 ;
-//         int j = i + 1;
-//         while(i < (nums.size()-1)){
-//             if(nums[i] == nums[j]){
-//                 return true ;
-               
-//             }
-//             j++;
-//             if(j > (nums.size()-1)){
-//                 i++;
-//                 j = i + 1 ;
-//             }
-//         }
-//         return false;
-//     }
-// };
