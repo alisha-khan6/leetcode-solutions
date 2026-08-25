e98f0a257eb57b7388ea6efc250128e411f55854
Alisha Khan <alishakhan61107@gmail.com>
2026-08-25 10:51:58 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array - Runtime - 0 ms (100.00%), Memory - 17.7 MB (18.75%)

--- solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,43 +1,42 @@
 class Solution {
+    int first(const vector<int>& nums , int n){
+        int low = 0 ;
+        int high = nums.size() - 1;
+        int a = -1;
+        while(low <= high){
+        int mid = (low + high)/2 ;
+        if(n == nums[mid]){
+            a = mid ;
+            high = mid-1;
+        }else if(n > nums[mid]){
+            low = mid + 1 ;
+        }else{
+            high = mid - 1 ;
+        }
+        }
+        return a ;
+    }
+    int lastindex(vector<int> &nums , int n){
+        int low = 0 ;
+        int high = nums.size() - 1;
+        int b=-1;
+        while(low <= high){
+        int mid = (low + high)/2 ;
+        if(n == nums[mid]){
+            b = mid ;
+            low=mid+1;
+        }else if(n > nums[mid]){
+            low = mid + 1 ;
+        }else{
+            high = mid - 1 ;
+        }
+        }
+        return b;
+    }
 public:
-    int first(vector<int>& nums, int target){
-    int ans = -1;
-    int low = 0 ;
-    int high = nums.size() - 1 ;
-    while(low <= high){
-        int mid = (low+high)/2;
-        if(nums[mid] == target){
-            ans = mid ;
-            high = mid - 1 ;
-        }else if (target > nums[mid]){
-            low = mid + 1 ;
-        }else{
-            high = mid - 1 ;
-        }
-    }
-    return ans ;
-    }
-    int last(vector<int>& nums, int target){
-    int ans = -1;
-    int low = 0 ;
-    int high = nums.size() -1 ;
-    while(low <= high){
-        int mid = (low+high)/2;
-        if(nums[mid] == target){
-            ans = mid ;
-            low = mid + 1 ;
-        }else if (target > nums[mid]){
-            low = mid + 1 ;
-        }else{
-            high = mid - 1 ;
-        }
-    }
-    return ans ;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-    int firstIndex = first(nums, target);
-    int lastIndex = last(nums, target);
-
-    return {firstIndex, lastIndex};
+        int a = first(nums,target) ;
+        int b = lastindex(nums,target) ;
+        return {a,b} ;
     }
 };
