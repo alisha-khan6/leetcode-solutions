e81f8100880785b20b3041da9ac93c9cd0d9832f
Alisha Khan <alishakhan61107@gmail.com>
2026-08-28 12:30:53 +0000

    Sync LeetCode submission - Sort Colors - Runtime - 0 ms (100.00%), Memory - 11.6 MB (79.16%)

--- solutions/0075-sort-colors/solution.cpp
+++ solutions/0075-sort-colors/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // int temp = arr[i] ;
-        for(int i = 1 ; i < nums.size() ; i++){
-            int temp = nums[i] ;
-            for(int j = i - 1 ; j >= 0 ; j-- ){
-                if(temp < nums[j]){
+        int n = nums.size() ;
+        for(int i = 1 ; i < n ; i++){
+            int key = nums[i] ;
+            int j ;
+            for(j = i - 1 ; j >= 0 ; j--){
+                if(key < nums[j]){
                     nums[j+1] = nums[j] ;
                 }else{
                     break ;
                 }
-                nums[j] = temp ;
-            } 
+            }
+            nums[j+1] = key ;
         }
-       
+        // return nums ;
     }
 };
