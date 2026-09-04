ed88a5a0a51857594395fdd8c00a16f63e1e588a
Alisha Khan <alishakhan61107@gmail.com>
2026-09-04 04:38:43 +0000

    Sync LeetCode submission - Search a 2D Matrix - Runtime - 0 ms (100.00%), Memory - 13.4 MB (44.53%)

---
+++ solutions/0074-search-a-2d-matrix/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // for(int i = 0 ; i < matrix.size() ; i++){
+        //     for(int j = i ; j < matrix[i].size() ; j++){
+        //         if(matrix[i][j] == target){
+        //             return true ;
+        //         }
+        //     }
+        // }
+        // return false ;
+        int m = matrix.size() ;
+        int n = matrix[0].size() ;
+        int low = 0 ; 
+        int high = (m*n) - 1 ;
+        bool found = false ;
+        while(low <= high){
+            int mid = (low + high) / 2 ;
+            int i = mid / n ;
+            int j = mid % n ;
+            if(matrix[i][j] == target){
+                return true ;
+            }else if(matrix[i][j] < target){
+                low = mid + 1 ;
+            }else{
+                high = mid - 1 ;
+            }
+        }
+        return false ;
+    }
+};
