2b980458078e975c82256973b49ed2641e89b780
Alisha Khan <alishakhan61107@gmail.com>
2026-09-07 18:09:33 +0000

    Sync LeetCode submission - Delete Node in a Linked List - Runtime - 5 ms (78.15%), Memory - 12.4 MB (56.55%)

--- solutions/0237-delete-node-in-a-linked-list/solution.cpp
+++ solutions/0237-delete-node-in-a-linked-list/solution.cpp
@@ -9,10 +9,10 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* temp = node->next;
-        node->val = temp->val;
-        node->next = temp->next;
-        
-        delete temp;
+        ListNode* temp = node -> next ;
+        node -> val = temp -> val ;
+        node -> next = temp -> next ;
+        delete temp ;
     }
 };
+// What we can do is : as head is not given we can just store address of next to the given node in temp . Then in given node we will store value of the next node it will become 4 1 1 9 , then what we will do we will point the given node to temp -> next ; and then delete the old node i.e is temp and it will become 4 1 9
